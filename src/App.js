3d2df75b202dedcbc74ed8141e7be396dfe823a5
rohan <rohanro74@gmail.com>
2020-12-05 05:18:26 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 function App() {
   return (
@@ -8,6 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from Rohan Sunki</h1>
+        <h1>We now have Auth!</h1>
       </header>
     </div>
   );
